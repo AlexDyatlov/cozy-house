c7b429e6d93ddf55b345e8d5bca98fb3646b0879
Alyx11 <aleks78.gooogle@gmail.com>
2020-05-05 16:18:23 +0300

    added slider

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,8 @@ gulp.task("style", function () {
 	return gulp
 		.src([
 			//указываем, где брать исходники
-			"node_modules/normalize.css/normalize.css",
+			'node_modules/normalize.css/normalize.css',
+			'node_modules/slick-carousel/slick/slick.css',
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat("libs.min.css")) //склеиваем их в один файл с указанным именем
@@ -107,7 +108,8 @@ gulp.task("script", function () {
 	return gulp
 		.src([
 			//тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
-			"node_modules/jquery/dist/jquery.js",
+			'node_modules/jquery/dist/jquery.js',
+			'node_modules/slick-carousel/slick/slick.js',
 		])
 		.pipe(size())
 		.pipe(sourcemaps.init())
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+  $('.pets-slider__inner').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slider-arrows slider-arrows__right"></button>',
+    prevArrow: '<button type="button" class="slider-arrows slider-arrows__left"></button>',
+  });
+  
 });

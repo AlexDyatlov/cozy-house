4827d9a778f909eae531cfb6d2e2e068766cc2ed
Alyx11 <aleks78.gooogle@gmail.com>
2020-05-08 16:24:01 +0300

    added footer

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,30 @@ $(function(){
     slidesToScroll: 1,
     nextArrow: '<button type="button" class="slider-arrows slider-arrows__right"></button>',
     prevArrow: '<button type="button" class="slider-arrows slider-arrows__left"></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 625,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+    ]
   });
   
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
 });
